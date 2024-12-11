f697bd60969487f8a3068a3e4650c9635a78d1c8
Oleksii Dovgodko <alexd.admin@gmail.com>
2024-12-11 16:07:47 +0100

    Added support for GDAL 3.10.0

--- source/lib/SmartPointers.h
+++ source/lib/SmartPointers.h
@@ -35,18 +35,33 @@ class _CustomDeleterBaseVoid
 		}
 };
 
+//Template class for smart pointer deleter objects that call functions which accept T* pointers
+template <typename TParam, typename TParamH, typename TRet, TRet(*DeleterFunc)(TParamH)>
+class _CustomDeleterBaseTRet
+{
+	public:
+		inline TRet operator()(TParam* t) {
+			return DeleterFunc(TParam::ToHandle(t));
+		}
+};
+
+
 //Boilerplate macros for declaring our smart pointer types
 #define _MERGETIFF_VOID_DELETER_SMART_POINTER(dtype, callback) typedef MERGETIFF_SMART_POINTER_TYPE<dtype, _CustomDeleterBaseVoid<callback>> dtype##Ref
 #define _MERGETIFF_T_DELETER_SMART_POINTER(dtype, callback) typedef MERGETIFF_SMART_POINTER_TYPE<dtype, _CustomDeleterBaseT<dtype, callback>> dtype##Ref
 #define _MERGETIFF_OPTS_DELETER_SMART_POINTER(dtype) typedef MERGETIFF_SMART_POINTER_TYPE<dtype, _CustomDeleterBaseT<dtype, dtype##Free>> dtype##Ref
+#define _MERGETIFF_T_DELETER_SMART_POINTER_RET(dtype, rettype, callback) typedef MERGETIFF_SMART_POINTER_TYPE<dtype, _CustomDeleterBaseTRet<dtype, dtype##H, rettype, callback>> dtype##Ref
 
 //Smart pointer type for raw C strings returned by the GDAL API
 //(Note that you could also use the CPLString class from <cpl_string.h> and its Seize() method to achieve the same RAII behaviour, albeit with a memory copy and more verbose syntax)
 typedef MERGETIFF_SMART_POINTER_TYPE<char[], _CustomDeleterBaseVoid<CPLFree>> CPLStringRef;
 
+#if GDAL_VERSION_NUM > GDAL_COMPUTE_VERSION(3, 5, 0)
 //Smart pointer type for GDAL datasets
-_MERGETIFF_VOID_DELETER_SMART_POINTER(GDALDataset, GDALClose);
-
+	_MERGETIFF_T_DELETER_SMART_POINTER_RET(GDALDataset, CPLErr, GDALClose);
+#else
+	_MERGETIFF_T_DELETER_SMART_POINTER(GDALDataset, GDALClose);
+#endif
 //Smart pointer type for OGR coordinate transformation objects
 _MERGETIFF_VOID_DELETER_SMART_POINTER(OGRCoordinateTransformation, OCTDestroyCoordinateTransformation);
 
